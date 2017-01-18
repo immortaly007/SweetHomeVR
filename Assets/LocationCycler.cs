5ba86b01eda09496e12b612d4f0822e96e72d944
Bas Dado <immortaly007@gmail.com>
2017-01-18 21:20:27 +0100

    Initial Commit

---
+++ Assets/LocationCycler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+public class LocationCycler : MonoBehaviour
+{
+    public GameObject player;
+    public List<Transform> playerLocations;
+
+    private int currentPlayerLocationIdx;
+    public int CurrentPlayerLocationIdx {
+        get
+        {
+            return this.currentPlayerLocationIdx;
+        }
+        set
+        {
+            currentPlayerLocationIdx = value % playerLocations.Count;
+            player.transform.position = playerLocations[currentPlayerLocationIdx].position;
+        }
+    }
+
+    public void Start()
+    {
+        MagnetSensor.OnCardboardTrigger += () => NextLocation();
+        currentPlayerLocationIdx = 0;
+    }
+
+    public void NextLocation()
+    {
+        CurrentPlayerLocationIdx++;
+    }
+}
